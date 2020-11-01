1a7058342d72e3fb5aec45299a5668d222c794a9
afefhnid <afefhnid@gmail.com>
2020-11-01 02:31:10 +0100

    api

--- launch.py
+++ launch.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 from tools import model
-from tools import nettoyage
-from tools import entrainement
 import json
 import numpy as np
 
@@ -16,28 +14,23 @@ def home():
 
 @app.route("/prediction", methods=['POST'])
 def prediction():
+    return render_template("prediction.html")
 
-    user_text1 = request.form.get('input_text2')
-    user_text2 = request.form.get('input_text3')
-    user_text3 = request.form.get('input_text4')
-    user_text4 = request.form.get('input_text5')
-    user_text5 = request.form.get('input_text6')
-    user_text6 = request.form.get('input_text7')
-    user_text7 = request.form.get('input_text8')
-    user_text8 = request.form.get('input_text9')
-    user_text9 = request.form.get('input_text10')
-    user_text10 = request.form.get('input_text11')
-    user_text11 = request.form.get('input_text12')
-    user_text12 = request.form.get('input_text13')
-    user_text13 = request.form.get('input_text14')
-    user_text14 = request.form.get('input_text15')
 
-    # user_text = [user_text1, user_text2, user_text3, user_text4, user_text5, user_text6, user_text7,
-    # user_text8, user_text9, user_text10, user_text11, user_text13, user_text13, user_text14]
-    user_text = np.array([1, 22, 222, 22, 44, 88, 564]).reshape(1, -1)
-    prediction = model(user_text)
+@app.route("/reponse", methods=['POST'])
+def reponse():
+    input_amt = request.form.get('input_amt')
+    input_gender = request.form.get('input_gender')
+    input_state = request.form.get('input_state')
+    input_city_pop = request.form.get('input_city_pop')
+    input_numCardLength = request.form.get('input_numCardLength')
+    input_age = request.form.get('input_age')
+
+    params = np.array([input_amt, input_gender, input_state,
+                       input_city_pop, input_numCardLength, input_age]).reshape(1, -1)
+    prediction = model(params)
    # return json.dumps({'text_user': prediction})
-    return render_template("reponse.html", input_text=user_text, prediction=prediction)
+    return render_template("reponse.html", prediction=prediction)
 
 
 if __name__ == "__main__":
